169cdd73a7d9d5b9d8b86e2672b612882fc345cb
Arseniy Sitnikov <74531830+arscoolik@users.noreply.github.com>
2023-05-20 02:47:39 +0300

    Update yandex_cloud_request.py
    Added upload to yandex cloud object storage

--- yandex_cloud_request.py
+++ yandex_cloud_request.py
@@ -1,18 +1,45 @@
 import requests
 import time
-name_list = ['mvd.mp3', 'Nout_f_4.mp3', 'out_a_1.mp3', 'out_a_2.mp3', 'out_a_2.mp3', 'Nout_a_1.mp3', 'Nout_a_2.mp3', 'Nout_a_4.mp3', 'Nout_a_9.mp3', 'Nout_a_10.mp3']
+import boto3
 
-for FILE_NAME in name_list:
-    url = 'https://transcribe.api.cloud.yandex.net/speech/stt/v2/longRunningRecognize'
-    headers = {"Authorization": "Bearer t1.9euelZrHmZHOksebnY-UlI6Sy5aZie3rnpWax4_JzpmMjIqdlYmMyozKnZLl8_c6PGBc-e8pWkFa_t3z93pqXVz57ylaQVr-zef1656VmsbKiZnMi5aWmomal8-MzJXI7_0.E3DgsuZZkwMBrwvgIcFpVMf2JjJvGpCu8dBfaIT5wvhzVFW-9VvVMe3h8dwj8zjYBcXQbsp-Hj3CC7rJRflrCg"}
-    myobj = {"config": {"specification": {"audioEncoding": "MP3"},},"audio": {"uri": "https://storage.yandexcloud.net/kaifyoucaught/" + FILE_NAME},}
-    x = requests.post(url, headers=headers, json=myobj)
-    operationID = x.json()["id"]
+ACCESS_KEY_UPLOAD = 'YCAJEslrAKKbLge1bCCRQfrkY'
+SECRET_KEY_UPLOAD = 'YCMFVTBsh5Oc8X3pi0J_BWOVHV6dIobigKS9rB1R'
+YANDEX_URL = 'https://storage.yandexcloud.net/'
+REGION = 'ru-central1'
+BEST_TEAM_EVER = 'kaifyoucaught'
+FILE_NAME = "senya.mp3"
+API_KEY = "t1.9euelZrHmZHOksebnY-UlI6Sy5aZie3rnpWax4_JzpmMjIqdlYmMyozKnZLl8_c6PGBc-e8pWkFa_t3z93pqXVz57ylaQVr-zef1656VmsbKiZnMi5aWmomal8-MzJXI7_0.E3DgsuZZkwMBrwvgIcFpVMf2JjJvGpCu8dBfaIT5wvhzVFW-9VvVMe3h8dwj8zjYBcXQbsp-Hj3CC7rJRflrCg"
+
+#uploading file to yandex cloud service
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=ACCESS_KEY_UPLOAD,
+    aws_secret_access_key=SECRET_KEY_UPLOAD,
+    endpoint_url=YANDEX_URL,
+    region_name=REGION
+)
+s3_client.upload_file(FILE_NAME, BEST_TEAM_EVER, FILE_NAME)
+time.sleep(5)
+#sending file to be analyzed
+url = 'https://transcribe.api.cloud.yandex.net/speech/stt/v2/longRunningRecognize'
+headers = {"Authorization": "Bearer " + API_KEY}
+myobj = {"config": {"specification": {"audioEncoding": "MP3"}, }, "audio": {"uri": YANDEX_URL + BEST_TEAM_EVER + "/" + FILE_NAME}}
+x = requests.post(url, headers=headers, json=myobj)
+operationID = x.json()["id"]
+
+#waiting for response
+time.sleep(25)
+if requests.get('https://operation.api.cloud.yandex.net/operations/' + operationID, headers=headers).json()["done"] == "false":
     time.sleep(25)
-    response = requests.get('https://operation.api.cloud.yandex.net/operations/' + operationID, headers=headers)
-    response = response.json()['response']
-    final_text = ""
-    for i in response['chunks']:
-        final_text += i['alternatives'][0]['text'] + "."
-    with open(FILE_NAME[:-4] + "_out.txt", "w", encoding="utf-8") as f:
-        f.write(final_text)
+    if requests.get('https://operation.api.cloud.yandex.net/operations/' + operationID, headers=headers).json()[
+        "done"] == "false":
+        time.sleep(25)
+
+#saving file as txt
+response = requests.get('https://operation.api.cloud.yandex.net/operations/' + operationID, headers=headers)
+response = response.json()['response']
+final_text = ""
+for i in response['chunks']:
+    final_text += i['alternatives'][0]['text'] + "."
+with open(FILE_NAME[:-4] + "_out.txt", "w", encoding="utf-8") as f:
+    f.write(final_text)
