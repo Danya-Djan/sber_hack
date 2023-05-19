6f91eb49bde50ba0a36fafb196341a6a215a9513
Danya-Djan <74513497+Danya-Djan@users.noreply.github.com>
2023-05-20 02:35:45 +0300

    Added converting file.2

--- convert.py
+++ convert.py
@@ -1,15 +1,27 @@
+import os
 from pydub import AudioSegment
 
 def convert_wav_to_mp3(wav_file, mp3_file):
-    # Load the WAV file
     audio = AudioSegment.from_wav(wav_file)
-
-    # Export the audio to MP3 format
     audio.export(mp3_file, format='mp3')
 
-# Specify the input WAV file and output MP3 file paths
-input_wav_file = 'input.wav'
-output_mp3_file = 'output.mp3'
+# Specify the input folder containing WAV files
+input_folder = 'train-samples/NotFraud'
 
-# Call the conversion function
-convert_wav_to_mp3(input_wav_file, output_mp3_file)
+# Specify the output folder to store the converted MP3 files
+output_folder = 'output/NotFraud'
+
+# Create the output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Get a list of WAV files in the input folder
+wav_files = [f for f in os.listdir(input_folder) if f.endswith('.wav')]
+
+# Convert each WAV file to MP3 format
+for wav_file in wav_files:
+    # Create the corresponding output file path
+    mp3_file = os.path.join(output_folder, os.path.splitext(wav_file)[0] + '.mp3')
+
+    # Call the conversion function
+    convert_wav_to_mp3(os.path.join(input_folder, wav_file), mp3_file)
